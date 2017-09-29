5fb856c888b8732648c46da3e6eb40c89494e78c
akodate <alex.kodate@gmail.com>
2017-09-29 17:29:12 +0700

    Add Firebase and non-functional login form

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBPkspixXQwoRs6AlZonPJuRAMezgpX0gc',
+      authDomain: 'auth-d1dfb.firebaseapp.com',
+      databaseURL: 'https://auth-d1dfb.firebaseio.com',
+      projectId: 'auth-d1dfb',
+      storageBucket: '',
+      messagingSenderId: '705419080916'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     );
   }
